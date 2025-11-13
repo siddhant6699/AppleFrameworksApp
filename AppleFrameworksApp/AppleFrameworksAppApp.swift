6c4376650a5233fed3ad0f5d6ad8db4113f571ec
siddhant <sidroxx.sc@gmail.com>
2025-11-13 16:19:36 +0530

    Initial Commit

---
+++ AppleFrameworksApp/AppleFrameworksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksAppApp.swift
+//  AppleFrameworksApp
+//
+//  Created by Siddhant Chouhan on 13/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
