bbd7a2b5dc231a05486fb140570b276227ffc004
siddhant <sidroxx.sc@gmail.com>
2025-11-19 21:55:48 +0530

    removed comment

--- AppleFrameworksApp/AppleFrameworksAppApp.swift
+++ AppleFrameworksApp/AppleFrameworksAppApp.swift
@@ -11,7 +11,6 @@ import SwiftUI
 struct AppleFrameworksAppApp: App {
     var body: some Scene {
         WindowGroup {
-//            FrameworkGridView()
             TabBarView()
         }
     }
